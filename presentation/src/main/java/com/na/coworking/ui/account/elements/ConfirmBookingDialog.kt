07b1f373a421ac911f8da20f21cfca0b7730cfc1
Flash-Kaa <egor3352@yandex.ru>
2024-07-21 18:41:09 +0500

    change keyboard type for confirm dialog

--- presentation/src/main/java/com/na/coworking/ui/account/elements/ConfirmBookingDialog.kt
+++ presentation/src/main/java/com/na/coworking/ui/account/elements/ConfirmBookingDialog.kt
@@ -16,6 +16,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -36,6 +37,7 @@ import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -193,6 +195,7 @@ private fun CodeField(
                 shape = RoundedCornerShape(5.dp)
             )
             .fillMaxWidth(),
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
         placeholder = { Placeholder() },
         colors = getTextFieldColors(isError),
         isError = isError.value
